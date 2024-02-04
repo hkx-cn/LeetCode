7d95bc6b7a072c0ab78e7987bdef36443cbed2da
hukaixiang-cn <18119602629@163.com>
2024-02-04 22:53:53 +0800

    commit

--- LeetCodeMain.cpp
+++ LeetCodeMain.cpp
@@ -1,10 +1,10 @@
 #include "LeetCodeMain.hpp"
-#include "LeetCode436.hpp"
+//#include "LeetCode436.hpp"
 int main()
 {
-    Solution solution;
-    vector<vector<int>> nums = {{1, 2}};
-    solution.findRightInterval(nums);
-    // std::cin.get();
-    return 0;
+//    Solution solution;
+//    vector<vector<int>> nums = {{1, 2}};
+//    solution.findRightInterval(nums);
+//    std::cin.get();
+     return 0;
 }
---
+++ archive/2024-02/LeetCode292.hpp
@@ -0,0 +1,5 @@
+#pragma once
+class Solution {
+public:
+    bool canWinNim(int n);
+};
