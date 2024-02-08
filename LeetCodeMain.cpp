b5bea143a52b8fc6fc5ee2682cdceca3646311a5
hukaixiang-cn <18119602629@163.com>
2024-02-08 18:44:51 +0800

    commit

--- LeetCodeMain.cpp
+++ LeetCodeMain.cpp
@@ -1,9 +1,10 @@
 #include "LeetCodeMain.hpp"
-#include "LeetCode43.hpp"
+#include "LeetCode73.hpp"
 int main()
 {
     Solution solution;
-    string mul = solution.multiply("9", "99");
+    vector<vector<int>> nums = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+    solution.setZeroes(nums);
 //    vector<vector<int>> nums = {{1, 2}};
 //    std::cin.get();
     return 0;
