a1ac1ad7b9c552e59fa8eb2ea1c2809119010c72
hkx-cn <18119602629@163.com>
2024-09-22 08:27:26 +0000

    commit

--- LeetCodeMain.c
+++
@@ -1,9 +0,0 @@
-#include "LeetCode34.h"
-#include "LeetCodeMain.h"
-int main()
-{
-    int a[] = {1};
-    int returnSize = 2;
-    int *result = searchRange(a, 1, 1, &returnSize);
-    return 0;
-}
---
+++ LeetCodeMain.cpp
@@ -0,0 +1,5 @@
+#include "LeetCodeMain.hpp"
+int main()
+{
+    return 0;
+}
