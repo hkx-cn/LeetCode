b596fdf45067b8682c49f732bbdd057ce46fcccb
hkx-cn <18119602629@163.com>
2024-11-10 21:07:49 +0800

    1422

--- LeetCodeMain.cpp
+++ LeetCodeMain.cpp
@@ -1,11 +1,9 @@
 #include "LeetCodeMain.hpp"
-#include "LeetCode4.hpp"
+#include "LeetCode1422.hpp"
 
 int main() {
-    vector<int> nums1 = {2};
-    vector<int> nums2 = {1, 3, 4, 5, 6};
-    Solution solution;
-    double result = solution.findMedianSortedArrays(nums1, nums2);
+    char s[7] = "011101";
+    int result = maxScore(s);
     std::cout << result << std::endl;
     return 0;
 }
--- archive/2024-11/LeetCode1422.hpp
+++ archive/2024-11/LeetCode1422.hpp
@@ -8,3 +8,5 @@ class Solution {
 public:
     int maxScore(string s);
 };
+
+int maxScore(char* s);
