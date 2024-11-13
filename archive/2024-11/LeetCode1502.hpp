bd30be1c227c4729dbec207bf6e3c017786c8d2c
hkx-cn <18119602629@163.com>
2024-11-13 22:06:39 +0800

    1502

--- archive/2024-11/LeetCode1502.hpp
+++ archive/2024-11/LeetCode1502.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
-#include <iostream>
 #include <vector> 
 #include <algorithm>
+#include <stdlib.h>
 using namespace std;
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr);
 };
+
+bool canMakeArithmeticProgression(int* arr, int arrSize);
