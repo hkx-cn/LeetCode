af602cd835b28c663844550c77f5856e5c6e5d23
hkx-cn <18119602629@163.com>
2024-11-18 16:49:05 +0800

    374

--- archive/2023-11/LeetCode374.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Solution {
-public:
-    int guessNumber(int n);
-    int guess(int num);
-};
---
+++ archive/2024-11/LeetCode374.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class Solution {
+public:
+    int guessNumber(int n);
+    int guess(int num);
+};
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return       -1 if num is higher than the picked number
+ *               1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+int guessNumber(int n);
