f3a0c222e9b9823ffd1d517831769895e952d700
hkx-cn <18119602629@163.com>
2024-12-05 20:18:00 +0800

    153

--- archive/2023-12/LeetCode153.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-using namespace std;
-class Solution
-{
-public:
-    int findMin(vector<int> &nums); 
-};
---
+++ archive/2024-12/LeetCode153.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums);
+};
+
+int findMin(int* nums, int numsSize);
