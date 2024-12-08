0f71692fb45582b7b4c20ccd930bc60e02efcc46
hkx-cn <18119602629@163.com>
2024-12-08 12:19:43 +0000

    21

--- archive/2024-12/LeetCode21.cpp
+++ archive/2024-12/LeetCode21.cpp
@@ -24,23 +24,24 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    struct ListNode* result = (struct ListNode*)malloc(sizeof(struct ListNode)), *next = result;
+    struct ListNode* result = (struct ListNode*)malloc(sizeof(struct ListNode)), *cur = result;
     while (list1 != NULL || list2 != NULL) {
         if (list1 == NULL) {
-            next->next = list2;
+            cur->next = list2;
             list2 = list2->next;
         } else if (list2 == NULL) {
-            next->next = list1;
+            cur->next = list1;
             list1 = list1->next;
         } else if (list1->val > list2->val) {
-            next->next = list2;
+            cur->next = list2;
             list2 = list2->next;
         } else if (list1->val <= list2->val) {
-            next->next = list1;
+            cur->next = list1;
             list1 = list1->next;
         }
-        next = next->next;
+        cur = cur->next;
     }
+    cur->next = NULL;
     return result->next;
 }
 
