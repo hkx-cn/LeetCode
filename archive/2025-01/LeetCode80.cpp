b8835e5a3247850757559fce37ecc1d64d008434
hkx-cn <18119602629@163.com>
2025-01-30 21:37:47 +0800

    80

--- archive/2025-01/LeetCode80.cpp
+++ archive/2025-01/LeetCode80.cpp
@@ -1,6 +1,8 @@
 // 80. 删除有序数组中的重复项 II
-// 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
-// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+// 给你一个有序数组 nums ，请你 原地
+// 删除重复出现的元素，使得出现次数超过两次的元素只出现两次
+// ，返回删除后数组的新长度。 不要使用额外的数组空间，你必须在 原地 修改输入数组
+// 并在使用 O(1) 额外空间的条件下完成。
 
 // 说明：
 // 为什么返回数值是整数，但输出的答案是数组呢？
@@ -29,30 +31,44 @@
 // nums 已按升序排列
 #include "LeetCode80.hpp"
 
-int Solution::removeDuplicates(vector<int> &nums)
-{
-    int idx = 0, temp = nums[0] - 1, count = 0;
-    for (size_t i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] == temp)
-        {
+int Solution::removeDuplicates(vector<int> &nums) {
+    int cur = nums[0], count = 0, idx = 0;
+    for (size_t i = 0; i < nums.size(); i++) {
+        if (nums[i] == cur) {
             count++;
-            if (count <= 2) 
-            {
-                swap(nums[i], nums[idx]);
-                idx++;
-            }
-        }
-        else
-        {
-            temp = nums[i];
-            swap(nums[i], nums[idx]);
+        } else {
+            cur = nums[i];
             count = 1;
-            idx++;
         }
+        if (count > 2) {
+            idx--;
+        } else if (idx != i) {
+            std::swap(nums[i], nums[idx]);
+        }
+        idx++;
     }
     return idx;
 }
+
+// int Solution::removeDuplicates(vector<int> &nums) {
+//     int idx = 0, temp = nums[0] - 1, count = 0;
+//     for (size_t i = 0; i < nums.size(); i++) {
+//         if (nums[i] == temp) {
+//             count++;
+//             if (count <= 2) {
+//                 swap(nums[i], nums[idx]);
+//                 idx++;
+//             }
+//         } else {
+//             temp = nums[i];
+//             swap(nums[i], nums[idx]);
+//             count = 1;
+//             idx++;
+//         }
+//     }
+//     return idx;
+// }
+
 // int Solution::removeDuplicates(vector<int> &nums)
 // {
 //     int tmp = nums[0], count = 1, len = 1;
@@ -81,11 +97,19 @@ int Solution::removeDuplicates(vector<int> &nums)
 // 方法一：双指针
 // 思路及解法
 // 在阅读本题前，读者们可以先尝试解决「26. 删除有序数组中的重复项」。
-// 因为给定数组是有序的，所以相同元素必然连续。我们可以使用双指针解决本题，遍历数组检查每一个元素是否应该被保留，如果应该被保留，就将其移动到指定位置。具体地，我们定义两个指针 idx 和 fast 分别为慢指针和快指针，其中慢指针表示处理出的数组的长度，快指针表示已经检查过的数组的长度，即 nums[fast] 表示待检查的第一个元素，nums[idx−1] 为上一个应该被保留的元素所移动到的指定位置。
-// 因为本题要求相同元素最多出现两次而非一次，所以我们需要检查上上个应该被保留的元素 nums[idx−2] 是否和当前待检查元素 nums[fast] 相同。当且仅当 nums[idx−2]=nums[fast] 时，当前待检查元素 nums[fast] 不应该被保留（因为此时必然有 nums[idx−2]=nums[idx−1]=nums[fast]）。最后，idx 即为处理好的数组的长度。
-// 特别地，数组的前两个数必然可以被保留，因此对于长度不超过 2 的数组，我们无需进行任何处理，对于长度超过 2 的数组，我们直接将双指针的初始值设为 2 即可。
-// class Solution {
-// public:
+// 因为给定数组是有序的，所以相同元素必然连续。我们可以使用双指针解决本题，遍历数组检查每一个元素是否应该被保留，如果应该被保留，就将其移动到指定位置。具体地，我们定义两个指针
+// idx 和 fast
+// 分别为慢指针和快指针，其中慢指针表示处理出的数组的长度，快指针表示已经检查过的数组的长度，即
+// nums[fast] 表示待检查的第一个元素，nums[idx−1]
+// 为上一个应该被保留的元素所移动到的指定位置。
+// 因为本题要求相同元素最多出现两次而非一次，所以我们需要检查上上个应该被保留的元素
+// nums[idx−2] 是否和当前待检查元素 nums[fast] 相同。当且仅当
+// nums[idx−2]=nums[fast] 时，当前待检查元素 nums[fast]
+// 不应该被保留（因为此时必然有 nums[idx−2]=nums[idx−1]=nums[fast]）。最后，idx
+// 即为处理好的数组的长度。
+// 特别地，数组的前两个数必然可以被保留，因此对于长度不超过 2
+// 的数组，我们无需进行任何处理，对于长度超过 2
+// 的数组，我们直接将双指针的初始值设为 2 即可。 class Solution { public:
 //     int removeDuplicates(vector<int>& nums) {
 //         int n = nums.size();
 //         if (n <= 2) {
@@ -105,3 +129,4 @@ int Solution::removeDuplicates(vector<int> &nums)
 // 复杂度分析
 // 时间复杂度：O(n)，其中 n 是数组的长度。我们最多遍历该数组一次。
 // 空间复杂度：O(1)。我们只需要常数的空间存储若干变量。
+
